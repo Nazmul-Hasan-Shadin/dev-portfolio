43f0f6dfca4314e0337ef1f8ab338d13f54d31f3
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-12-12 15:45:09 +0600

    project live link has added

--- src/Pages/Home/Projects/Project.jsx
+++ src/Pages/Home/Projects/Project.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 
 
 const Project = ({allproject}) => {
+  console.log(allproject.links)
     return (
 <div className="card card-compact bg-base-100 shadow-xl">
   <figure><img className="h-[230px]" src={allproject?.image} alt="Shoes" /></figure>
@@ -10,7 +12,7 @@ const Project = ({allproject}) => {
     <span>Type: {allproject?.type}</span>
     <p>If a dog chews shoes whose shoes does he choose?</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+     <Link to={allproject?.links}> <button className="btn btn-primary"> Visit Now </button></Link>
     </div>
   </div>
 </div>
