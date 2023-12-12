a1652f79537ec0a9ad6f5592e0de18b18263fdec
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-12-12 15:35:57 +0600

    footer has added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import Footer from '../Pages/Footer/Footer';
 
 const Main = () => {
     const links= <>
@@ -22,7 +23,7 @@ const Main = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
               </label>
             </div> 
-            <div className="flex-1 px-2 mx-2">Shadin Dev</div>
+            <div className="flex-1 px-2 mx-2"> <span className='font-bold'>Shadin Dev</span> </div>
             <div className="flex-none hidden lg:block">
               <ul className="menu menu-horizontal">
                 {/* Navbar menu content here */}
@@ -32,7 +33,16 @@ const Main = () => {
             </div>
           </div>
           {/* Page content here */}
-         <Outlet></Outlet>
+        { 
+       <div>
+
+<Outlet></Outlet>
+         <Footer></Footer>
+        </div>
+        
+        }
+
+        
         </div> 
         <div className="drawer-side">
           <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label> 
